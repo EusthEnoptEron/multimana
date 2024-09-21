5499633675842103a6f8866bc51cf1114780593d
EusthEnoptEron <umikarayattekita@yahoo.co.jp>
2024-09-21 21:18:42 +0200

    feat: initialize 2nd character according to python code

--- src/multiplayer/mod.rs
+++ src/multiplayer/mod.rs
@@ -1,22 +1,23 @@
-use crate::utils::{Message, EventHandler, Mod, TrampolineWrapper};
+use crate::utils::{EventHandler, Message, Mod, TrampolineWrapper};
 use anyhow::{anyhow, bail, Context, Result};
 use libmem::Address;
+use manasdk::core_u_object::UFunction;
 use manasdk::engine::{AActor, APawn, UGameplayStatics, UWorld};
 use manasdk::engine_settings::{ETwoPlayerSplitScreenType, UGameMapsSettings};
-use manasdk::{EFunctionFlags, FFrame, FNativeFuncPtr, HasClassObject, TFixedSizeArray, UClass, UObject, UObjectPointer};
+use manasdk::py_char_base::APyCharBase;
+use manasdk::x21::{AActGameState, USakuraBlueprintFunctionLibrary};
+use manasdk::x21_player_state::APyX21PlayerState;
+use manasdk::{FFrame, FNativeFuncPtr, HasClassObject, TFixedSizeArray, UObject, UObjectPointer};
 use std::any::Any;
 use std::ffi::c_void;
-use std::ops::DerefMut;
 use std::sync::RwLock;
-use tracing::{info, instrument, span, warn, Level};
-use manasdk::core_u_object::{FVector, UFunction};
-use manasdk::x21::{UActCharacterMovementComponent, USakuraBlueprintFunctionLibrary};
+use tracing::{error, info, instrument, warn};
 
 #[derive(Default)]
 struct MultiplayerData {
     initialized: bool,
     exec_function: Option<TrampolineWrapper<FNativeFuncPtr>>,
-    pawn: UObjectPointer<APawn>,
+    pawn: UObjectPointer<APyCharBase>,
 }
 
 #[derive(Default)]
@@ -48,9 +49,9 @@ impl Mod for MultiplayerMod {
 
     fn init(&self) -> Result<()> {
         info!("Loading multiplayer mod");
-        
-        let function: &UFunction = UObject::find_function(|it| it.function_flags.contains(EFunctionFlags::BlueprintEvent))
-            .context("Unable to find entry function")?; 
+
+        let function: &UFunction = UObject::find_function(|it| it.name() == "ExecutePythonScript")
+            .context("Unable to find entry function")?;
 
         fn on_exec_function(context: &UObject, stack: &FFrame, result: *mut c_void) {
             let _ = MultiplayerMod::call_in_place(|this| {
@@ -59,27 +60,30 @@ impl Mod for MultiplayerMod {
             });
         }
 
-        self.inner.write().map_err(|_| anyhow!("Unable to set exec func"))?.exec_function.replace(
-            unsafe {
-                libmem::hook_code(function.exec_function as Address, on_exec_function as Address)
-                    .context("Unable to hook into exec function")?
-                    .into()
-            }
-        );
+        self.inner
+            .write()
+            .map_err(|_| anyhow!("Unable to set exec func"))?
+            .exec_function
+            .replace(unsafe {
+                libmem::hook_code(
+                    function.exec_function as Address,
+                    on_exec_function as Address,
+                )
+                .context("Unable to hook into exec function")?
+                .into()
+            });
         info!("Hooked into exec function");
         Ok(())
     }
 
     fn tick(&self) -> Result<()> {
-        if self.inner.read().ok().context("Could not read data")?.initialized {
-            if let Some(pawn) = self.inner.read().ok().and_then(|it| it.pawn.clone().try_get().ok()) {
-                USakuraBlueprintFunctionLibrary::set_ai_controller_tick_enabled(pawn, false);
-                USakuraBlueprintFunctionLibrary::set_ai_controller_tick_interval(pawn, 0f32);
-                // let movement_component = pawn.get_movement_component().try_get().context("No movement comp")?.cast_mut::<UActCharacterMovementComponent>().context("Not an ActCharMovComp")?;
-                // movement_component.set_component_tick_enabled(true);
-                // movement_component.set_component_tick_interval(0.016666668f32);
-            }
-
+        if self
+            .inner
+            .read()
+            .ok()
+            .context("Could not read data")?
+            .initialized
+        {
             return Ok(());
         }
 
@@ -89,9 +93,13 @@ impl Mod for MultiplayerMod {
         let pawn = UGameplayStatics::get_player_pawn(world, 0);
 
         if let (Some(controller), Some(pawn)) = (player_controller.as_ref(), pawn.as_ref()) {
-            if pawn.class.as_ref().unwrap().name() == "BP_P001c_C" {
+            if pawn.class.as_ref().unwrap().name().starts_with("BP_P00") {
                 info!("Initializing");
-                self.inner.write().ok().context("Could not lock data")?.initialized = true;
+                self.inner
+                    .write()
+                    .ok()
+                    .context("Could not lock data")?
+                    .initialized = true;
                 if let Err(e) = self.try_enable_split_screen(pawn, world) {
                     warn!("Failed to enable split screen: {:?}", e);
                 } else {
@@ -123,28 +131,69 @@ impl MultiplayerMod {
         settings.b_offset_player_gamepad_ids = false;
         settings.two_player_splitscreen_layout = ETwoPlayerSplitScreenType::Vertical;
 
-
         info!("Creating player");
         let second_player = UGameplayStatics::create_player(world, 1, true)
-            .try_get().context("Unable to create second player")?;
-        info!("Created player");
-
+            .try_get()
+            .context("Unable to create second player")?;
+        info!("Created player: {}", second_player.class_hierarchy());
 
         info!("Creating array");
-        let mut actors = TFixedSizeArray::<&AActor, 5000usize>::new();
+        let mut actors = TFixedSizeArray::<&AActor, 50usize>::new();
         //let mut actors = TArray::<&AActor>::default();
         info!("Gettings actors");
-        UGameplayStatics::get_all_actors_of_class(world, APawn::static_class().into(), actors.as_mut());
 
-        info!("Actor count: {} (capacity={})", actors.len(), actors.max_elements);
-        if let Some(actor) = actors.iter().find(|it| it.class.as_ref().unwrap().name() == "BP_P002_C") {
-            let pawn = actor.cast().context("Unable to cast actor to pawn")?;
-            second_player.possess(pawn);
-            if let Ok(mut inner) = self.inner.write() {
-                inner.pawn = UObjectPointer::from(pawn);
+        UGameplayStatics::get_all_actors_of_class(
+            world,
+            APyCharBase::static_class().into(),
+            actors.as_mut(),
+        );
+
+        info!(
+            "Actor count: {} (capacity={})",
+            actors.len(),
+            actors.max_elements
+        );
+
+        if let Some(actor) = actors
+            .iter()
+            .find(|it| it.class.as_ref().unwrap().name() == "BP_P001c_C")
+        {
+            let pawn = actor
+                .cast::<APyCharBase>()
+                .context("Unable to cast actor to pawn")?;
+            let mut player_state = pawn.player_state.clone();
+
+            if let (Some(state), Some(next_target)) =
+                (second_player.player_state.as_ref(), player_state.as_ref())
+            {
+                USakuraBlueprintFunctionLibrary::exchange_player_state_unique_id(
+                    state,
+                    next_target,
+                );
+            } else {
+                error!("Whoopsie");
             }
-        } else {
-            bail!("No other pawns found!");
+
+            second_player.player_state = player_state.clone();
+            second_player.possess(pawn);
+
+            let game_state = UGameplayStatics::get_game_state(world)
+                .try_get()
+                .ok()
+                .and_then(|it| it.cast::<AActGameState>())
+                .context("Unable to get game state")?;
+
+            if let Some(state) = player_state
+                .as_mut()
+                .and_then(|it| it.cast_mut::<APyX21PlayerState>())
+            {
+                state.bit_set_b_is_a_bot(false);
+                game_state.register_player(state);
+            }
+
+            if let Ok(mut inner) = self.inner.write() {
+                inner.pawn = pawn.into();
+            }
         }
 
         Ok(())
